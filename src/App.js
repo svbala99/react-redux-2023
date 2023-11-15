659613d08fca9250c0b7b3f8050c05f8a25fd523
Balamurugan V <balamurugan@divum.in>
2023-11-15 12:41:08 +0530

    supplied redux data to UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { triggerIncrementRequest } from "./redux/actions/countAction";
 
-function App() {
+const App = () => {
+  const countFromRedux = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+  const { number, loading } = countFromRedux;
+
+  const handleIncrement = () => {
+    dispatch(triggerIncrementRequest());
+  };
+
+  const handleDecrement = () => {
+    alert("decrement functionality is pending yet");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container} className="App">
+      <button style={styles.btn} onClick={handleIncrement}>
+        +
+      </button>
+      {loading ? <h1> ...</h1> : <h1 style={styles.m48}>{number}</h1>}
+      <button style={styles.btn} onClick={handleDecrement}>
+        -
+      </button>
     </div>
   );
-}
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  btn: { width: 48, height: 48, borderRadius: 24 },
+  m48: { margin: 48 },
+};
 
 export default App;
