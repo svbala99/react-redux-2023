62034bd6802d9474c5429034d8728128af982684
sureshthangavel <sureshthangavel@divum.in>
2024-01-04 15:44:19 +0530

    calender ui done

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,65 +1,132 @@
-import React, {useEffect, useState} from 'react';
-import API_URLS from '../../constants/apiConstants';
-import moment from 'moment';
-
-
+import React, { useEffect, useState } from "react";
+import API_URLS from "../../constants/apiConstants";
+import { YearPicker, MonthPicker, DayPicker } from "react-dropdown-date";
 
 function Calender() {
-  const [currentDate, setCurrentDate] = useState(formatDate(moment()));
+  const [currentDate, setCurrentDate] = useState();
+  const [dateCalender, setDateCalender] = useState({
+    year: "",
+    month: "",
+    day: "",
+  });
+  const currDate = Date.now();
+  const date = new Date(currDate);
+  const [year, setYear] = useState(date.getFullYear());
+  const [month, setMonth] = useState(
+    String(date.getMonth() + 1).padStart(2, "0")
+  );
+  const [day, setDay] = useState(date.getDate());
 
   useEffect(() => {
-    const todayDate = moment().format('YYYY/DDMMYYYY');
+    const todayDate = `${day}${month}${year}`;
     setCurrentDate(todayDate);
-  }, []);
+  }, [day]);
 
-  function formatDate(date) {
-    if (!date.isValid()) {
-      return ''; // Handle invalid date
-    }
-
-    const day = date.format('D');
-    console.log(day,'day--------->')
-    const formattedDay = day.length === 1 ? `${day}` : day;
-    return date.format(`Y/${formattedDay}MMYYYY`);
-  }
-
-  const todayClick = () =>{
-    const todayDate = moment().format('YYYY/DDMMYYYY');
+  const todayClick = () => {
+    setDay(date.getDate());
+    const todayDate = `${day}${month}${year}`;
     setCurrentDate(todayDate);
-  }
+  };
 
-  const nextClick = () =>{
-    const newDate = moment(currentDate, 'Y/DMMYYYY').add(1, 'day');
-    setCurrentDate(formatDate(newDate));
-    console.log(currentDate,'currentdatenext')
-  }
-
-  const previousClick = () =>{
-    const newDate = moment(currentDate, 'YYYY/DMMYYYY').add(-1, 'day').format('YYYY/DMMYYYY');
+  const nextClick = () => {
+    setDay(day + 1);
+    const newDate = `${day}${month}${year}`;
     setCurrentDate(newDate);
-    console.log(currentDate,'currentdatenext')
-  }
+  };
+
+  const previousClick = () => {
+    setDay(day - 1);
+    const newDate = `${day}${month}${year}`;
+    setCurrentDate(newDate);
+  };
+
+  const submit = () => {
+    setYear(dateCalender.year);
+    if (dateCalender.month.length === 1) {
+      setMonth("0" + dateCalender.month);
+    } else {
+      setMonth(dateCalender.month);
+    }
+    setDay(dateCalender.day);
+    const newDate = `${day}${month}${year}`;
+    setCurrentDate(newDate);
+  };
 
   return (
     <div>
-      <img src={`${API_URLS.CALENDER}${currentDate}.jpg`} />
-      <button onClick={()=>{
-        todayClick()
-      }}>
+      <img src={`${API_URLS.CALENDER}${year}/${currentDate}.jpg`} />
+      <button
+        onClick={() => {
+          todayClick();
+        }}
+      >
         Today
       </button>
-      <button onClick={()=>{
-        previousClick()
-      }}>
+      <button
+        onClick={() => {
+          previousClick();
+        }}
+      >
         Previous
       </button>
-      <button onClick={()=>{
-        nextClick()
-      }}>
+      <button
+        onClick={() => {
+          nextClick();
+        }}
+      >
         Next
       </button>
+      <div className="test">
+        <MonthPicker
+          defaultValue={"MM"}
+          numeric // to get months as numbers
+          endYearGiven // mandatory if end={} is given in YearPicker
+          year={dateCalender.year} // mandatory
+          value={dateCalender.month} // mandatory
+          onChange={(month) => {
+            // mandatory
+            setDateCalender((prev) => ({ ...prev, month }));
+          }}
+          id={"month"}
+          // classes={`dropdown ${dayError ? "error" : ""}`}
+          optionClasses={"option"}
+        />
+        <DayPicker
+          defaultValue={"DD"}
+          year={dateCalender.year} // mandatory
+          month={dateCalender.month} // mandatory
+          endYearGiven // mandatory if end={} is given in YearPicker
+          value={dateCalender.day} // mandatory
+          onChange={(day) => {
+            // mandatory
+            setDateCalender((prev) => ({ ...prev, day }));
+          }}
+          id={"day"}
+          // classes={`dropdown ${monthError ? "error" : ""}`}
+          optionClasses={"option"}
+        />
+
+        <YearPicker
+          defaultValue={"YYYY"}
+          start={1994} // default is 1900
+          end={2024} // default is current year
+          reverse // default is ASCENDING
+          value={dateCalender.year} // mandatory
+          onChange={(year) => {
+            // mandatory
+            setDateCalender((prev) => ({ ...prev, year }));
+          }}
+          id={"year"}
+          // classes={`dropdown ${yearError ? "error" : ""}`}
+          optionClasses={"option"}
+        />
+
+        <button type="button" onClick={() => submit()}>
+          Submit
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Calender
+export default Calender;
