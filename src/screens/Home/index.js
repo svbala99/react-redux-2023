159c7b6c7b7430034b1e92f17e910441bd4eb220
sureshthangavel <sureshthangavel@divum.in>
2024-01-04 17:43:56 +0530

    calender ui date picker functionality done

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,6 +4,7 @@ import { YearPicker, MonthPicker, DayPicker } from "react-dropdown-date";
 
 function Calender() {
   const [currentDate, setCurrentDate] = useState();
+  const [todayDate, setTodayDate] = useState(new Date());
   const [dateCalender, setDateCalender] = useState({
     year: "",
     month: "",
@@ -20,6 +21,7 @@ function Calender() {
   useEffect(() => {
     const todayDate = `${day}${month}${year}`;
     setCurrentDate(todayDate);
+    setTodayDate(new Date());
   }, [day]);
 
   const todayClick = () => {
@@ -55,75 +57,82 @@ function Calender() {
   return (
     <div>
       <img src={`${API_URLS.CALENDER}${year}/${currentDate}.jpg`} />
-      <button
-        onClick={() => {
-          todayClick();
-        }}
-      >
-        Today
-      </button>
-      <button
-        onClick={() => {
-          previousClick();
-        }}
-      >
-        Previous
-      </button>
-      <button
-        onClick={() => {
-          nextClick();
-        }}
-      >
-        Next
-      </button>
-      <div className="test">
-        <MonthPicker
-          defaultValue={"MM"}
-          numeric // to get months as numbers
-          endYearGiven // mandatory if end={} is given in YearPicker
-          year={dateCalender.year} // mandatory
-          value={dateCalender.month} // mandatory
-          onChange={(month) => {
-            // mandatory
-            setDateCalender((prev) => ({ ...prev, month }));
-          }}
-          id={"month"}
-          // classes={`dropdown ${dayError ? "error" : ""}`}
-          optionClasses={"option"}
-        />
-        <DayPicker
-          defaultValue={"DD"}
-          year={dateCalender.year} // mandatory
-          month={dateCalender.month} // mandatory
-          endYearGiven // mandatory if end={} is given in YearPicker
-          value={dateCalender.day} // mandatory
-          onChange={(day) => {
-            // mandatory
-            setDateCalender((prev) => ({ ...prev, day }));
-          }}
-          id={"day"}
-          // classes={`dropdown ${monthError ? "error" : ""}`}
-          optionClasses={"option"}
-        />
+      <p>
+        {todayDate.toDateString()} {todayDate.toLocaleTimeString()}
+      </p>
+      <div className="calender-action-button">
+        <div>
+          <button
+            onClick={() => {
+              previousClick();
+            }}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => {
+              todayClick();
+            }}
+          >
+            Today
+          </button>
+          <button
+            onClick={() => {
+              nextClick();
+            }}
+          >
+            Next
+          </button>
+        </div>
+        <div className="test">
+          <MonthPicker
+            defaultValue={"MM"}
+            numeric // to get months as numbers
+            endYearGiven // mandatory if end={} is given in YearPicker
+            year={dateCalender.year} // mandatory
+            value={dateCalender.month} // mandatory
+            onChange={(month) => {
+              // mandatory
+              setDateCalender((prev) => ({ ...prev, month }));
+            }}
+            id={"month"}
+            // classes={`dropdown ${dayError ? "error" : ""}`}
+            optionClasses={"option"}
+          />
+          <DayPicker
+            defaultValue={"DD"}
+            year={dateCalender.year} // mandatory
+            month={dateCalender.month} // mandatory
+            endYearGiven // mandatory if end={} is given in YearPicker
+            value={dateCalender.day} // mandatory
+            onChange={(day) => {
+              // mandatory
+              setDateCalender((prev) => ({ ...prev, day }));
+            }}
+            id={"day"}
+            // classes={`dropdown ${monthError ? "error" : ""}`}
+            optionClasses={"option"}
+          />
 
-        <YearPicker
-          defaultValue={"YYYY"}
-          start={1994} // default is 1900
-          end={2024} // default is current year
-          reverse // default is ASCENDING
-          value={dateCalender.year} // mandatory
-          onChange={(year) => {
-            // mandatory
-            setDateCalender((prev) => ({ ...prev, year }));
-          }}
-          id={"year"}
-          // classes={`dropdown ${yearError ? "error" : ""}`}
-          optionClasses={"option"}
-        />
+          <YearPicker
+            defaultValue={"YYYY"}
+            start={2005} // default is 1900
+            end={2024} // default is current year
+            reverse // default is ASCENDING
+            value={dateCalender.year} // mandatory
+            onChange={(year) => {
+              // mandatory
+              setDateCalender((prev) => ({ ...prev, year }));
+            }}
+            id={"year"}
+            // classes={`dropdown ${yearError ? "error" : ""}`}
+            optionClasses={"option"}
+          />
 
-        <button type="button" onClick={() => submit()}>
-          Submit
-        </button>
+          <button type="button" onClick={() => submit()}>
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
